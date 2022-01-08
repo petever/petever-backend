6cdb96eaf5ac5285307e79d5efb93be6b1c4ab18
rumor1993 <rumor1993@naver.com>
2022-01-08 11:46:20 +0900

    petever project init

---
+++ src/main/java/com/example/petever/PeteverApplication.java
@@ -0,0 +1,13 @@
+package com.example.petever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeteverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeteverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petever/PeteverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeteverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
