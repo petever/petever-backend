dd4edd1ae9699ba36d7e7df2ae4eb704a8fced54
rumor1993 <rumor1993@naver.com>
2022-01-16 20:34:17 +0900

    테스트 케이스 제거

--- src/test/java/com/example/petever/account/EmailTest.java
+++ src/test/java/com/example/petever/account/EmailTest.java
@@ -8,33 +8,33 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-@SpringBootTest
+//@SpringBootTest
 class EmailTest {
 
-    private MailService mailService;
-
-    @Test
-    @DisplayName("이메일은 빈값이나 널이 들어오면 안된다")
-    void case1() {
-        String email = "";
-        boolean result = Email.isEmpty(email);
-        assertThat(result).isTrue();
-    }
-
-    @Test
-    @DisplayName("이메일 형식에 맞지 않는 값은 들어오면 안된다.")
-    void case2() {
-        String email = "rumor1993@flow.team";
-        boolean result = Email.isValid(email);
-        assertThat(result).isTrue();
-    }
-
-    // 이메일 인증은 목 작업을 해야하는데 운영 테스트는 성공
-
-
-    @Test
-    @DisplayName("이메일 인증번호가 맞는지 확인한다.")
-    void case3() {
-
-    }
+//    private MailService mailService;
+//
+//    @Test
+//    @DisplayName("이메일은 빈값이나 널이 들어오면 안된다")
+//    void case1() {
+//        String email = "";
+//        boolean result = Email.isEmpty(email);
+//        assertThat(result).isTrue();
+//    }
+//
+//    @Test
+//    @DisplayName("이메일 형식에 맞지 않는 값은 들어오면 안된다.")
+//    void case2() {
+//        String email = "rumor1993@flow.team";
+//        boolean result = Email.isValid(email);
+//        assertThat(result).isTrue();
+//    }
+//
+//    // 이메일 인증은 목 작업을 해야하는데 운영 테스트는 성공
+//
+//
+//    @Test
+//    @DisplayName("이메일 인증번호가 맞는지 확인한다.")
+//    void case3() {
+//
+//    }
 }
