c3fcf2e3b820467c04753ad333798c569312a2a5
rumor1993 <rumor1993@naver.com>
2022-01-16 20:54:09 +0900

    테스트 에러 부분 처리

--- src/test/java/com/example/petever/account/NicknameTest.java
+++ src/test/java/com/example/petever/account/NicknameTest.java
@@ -7,21 +7,21 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class NicknameTest {
-//    @Test
-//    @DisplayName("닉네임 빈값이나 널이 들어오면 안된다")
-//    void case1() {
-//        String name = "";
-//        Nickname nickname = new Nickname();
-//        boolean result = nickname.isEmpty(name);
-//        assertThat(result).isTrue();
-//    }
-//
-//    @Test
-//    @DisplayName("숫자, 영어, 한국어와 언더스코어를 허용하며 최소 2자 이상의 닉네임")
-//    void case2() {
-//        String name = "문혁_a12";
-//        Nickname nickname = new Nickname();
-//        boolean result = nickname.isValid(name);
-//        assertThat(result).isTrue();
-//    }
+    @Test
+    @DisplayName("닉네임 빈값이나 널이 들어오면 안된다")
+    void case1() {
+        String name = "";
+        Nickname nickname = new Nickname();
+        boolean result = nickname.isEmpty(name);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("숫자, 영어, 한국어와 언더스코어를 허용하며 최소 2자 이상의 닉네임")
+    void case2() {
+        String name = "문혁_a12";
+        Nickname nickname = new Nickname();
+        boolean result = nickname.isValid(name);
+        assertThat(result).isTrue();
+    }
 }
--- src/test/java/com/example/petever/account/PasswordTest.java
+++ src/test/java/com/example/petever/account/PasswordTest.java
@@ -8,19 +8,19 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class PasswordTest {
 
-//    @Test
-//    @DisplayName("비밀번호는 빈값이나 널이 들어오면 안된다.")
-//    void case1() {
-//        String pw = "";
-//        Password password = new Password();
-//        assertThat(password.isEmpty(pw)).isTrue();
-//    }
-//
-//    @Test
-//    @DisplayName("비밀번호는 형식에 맞지 않는 값은 들어오면 안된다.")
-//    void case2() {
-//        String pw = "1234123s4";
-//        Password password = new Password();
-//        assertThat(password.isValid(pw)).isTrue();
-//    }
+    @Test
+    @DisplayName("비밀번호는 빈값이나 널이 들어오면 안된다.")
+    void case1() {
+        String pw = "";
+        Password password = new Password();
+        assertThat(password.isEmpty(pw)).isTrue();
+    }
+
+    @Test
+    @DisplayName("비밀번호는 형식에 맞지 않는 값은 들어오면 안된다.")
+    void case2() {
+        String pw = "1234123s4";
+        Password password = new Password();
+        assertThat(password.isValid(pw)).isTrue();
+    }
 }
