21eb213bd7f3d1ce84e3a26898a977444267fa14
rumor1993 <rumor1993@naver.com>
2022-01-28 00:20:05 +0900

    로그인 컨트롤ㄹ러 CORS 처리

--- src/main/java/com/example/petever/account/controller/LoginController.java
+++ src/main/java/com/example/petever/account/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.example.petever.account.dto.UserDto;
 import com.example.petever.account.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LoginController {
     private final LoginService loginService;
 
