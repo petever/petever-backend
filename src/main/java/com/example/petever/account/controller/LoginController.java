a9f6396bf52efde07f9c489a5945050e725d3866
rumor1993 <rumor1993@naver.com>
2022-02-06 20:07:47 +0900

    세션 쿠키 처리

--- src/main/java/com/example/petever/account/controller/LoginController.java
+++ src/main/java/com/example/petever/account/controller/LoginController.java
@@ -3,11 +3,13 @@ package com.example.petever.account.controller;
 import com.example.petever.account.dto.LoginDto;
 import com.example.petever.account.dto.UserDto;
 import com.example.petever.account.service.LoginService;
+import com.example.petever.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @RestController
@@ -15,12 +17,14 @@ import javax.servlet.http.HttpSession;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LoginController {
     private final LoginService loginService;
+    private final SessionManager sessionManager;
 
     @PostMapping("/login")
-    public LoginDto login(@RequestBody LoginDto loginDto, HttpServletRequest request) {
+    public LoginDto login(@RequestBody LoginDto loginDto, HttpServletRequest request, HttpServletResponse response) {
         LoginDto loginUser = loginService.login(loginDto);
         HttpSession session = request.getSession();
         session.setAttribute("users", loginUser);
+        sessionManager.createSession(loginDto, response);
         return loginUser;
     }
 
@@ -28,6 +32,8 @@ public class LoginController {
     public String logout(@RequestBody UserDto userDto, HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         if (session != null) session.invalidate();
+
+        sessionManager.expire(request);
         return "로그아웃";
     }
 }
