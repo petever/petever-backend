d337b8fc31fe19baa58efdbd6972fa6f76bb080e
rumor1993 <rumor1993@naver.com>
2022-02-07 23:03:43 +0900

    로그인 쿠키 처리 안되는 이슈

--- src/main/java/com/example/petever/config/WebConfig.java
+++ src/main/java/com/example/petever/config/WebConfig.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@EnableJpaAuditing
-//@Configuration
+@EnableJpaAuditing
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
