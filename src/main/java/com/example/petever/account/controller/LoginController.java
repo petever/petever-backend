273f509083b0910634268e8a9d25c9d15f4b4955
rumor1993 <rumor1993@naver.com>
2022-02-08 00:34:46 +0900

    로그인 세션처리

--- src/main/java/com/example/petever/account/controller/LoginController.java
+++ src/main/java/com/example/petever/account/controller/LoginController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpSession;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "false")
 public class LoginController {
     private final LoginService loginService;
     private final SessionManager sessionManager;
