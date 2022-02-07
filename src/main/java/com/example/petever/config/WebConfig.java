d3a634988f68cfc4a673f253bb209c63cfa0c323
rumor1993 <rumor1993@naver.com>
2022-02-08 00:26:18 +0900

    로그인 세션처리

--- src/main/java/com/example/petever/config/WebConfig.java
+++ src/main/java/com/example/petever/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.example.petever.config;
 import com.example.petever.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,5 +21,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(2)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/login", "/logout");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowCredentials(false);
     }
 }
