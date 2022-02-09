aa6c3b1cd1197180d2a04b6c18fd43fb36c9ac48
rumor1993 <rumor1993@naver.com>
2022-02-09 23:21:10 +0900

    Board 컨트롤러 추가

---
+++ src/main/java/com/example/petever/account/controller/BoardController.java
@@ -0,0 +1,12 @@
+package com.example.petever.account.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class BoardController {
+
+}
